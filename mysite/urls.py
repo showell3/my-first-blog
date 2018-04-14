815fb24b4830f443403476cd15e3f29c7a92c19b
Your Name <showell3@nd.edu>
2018-04-14 17:09:59 -0400

    Added views to create / edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+
+
+
 ]
